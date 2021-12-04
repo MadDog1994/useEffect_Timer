2d45191f8cc1ffa9d1df5f9ff6399c658452e8fc
Ostap <flaminis000111@gmail.com>
2021-12-04 11:06:28 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Timer from './Components/Timer/timer.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Timer onTick={(seconds) => console.log('There is time left ' + seconds)}
+        time={8000}
+        step={1000}
+        autostart={false}
+      />
     </div>
   );
 }
